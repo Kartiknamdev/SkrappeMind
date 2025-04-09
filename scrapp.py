b86ad2f8a6067fee9e1834d2047664b854b360c5
Kartik Namdev <137074717+Kartiknamdev@users.noreply.github.com>
2025-04-09 14:36:12 +0530

    Update scrapp.py

--- scrapp.py
+++ scrapp.py
@@ -80,7 +80,7 @@ def cleanBC(body_content):
     )
     return cleaned_content
 
-def split_dom_content(dom_content, max_length=6000):
+def split_dom_content(dom_content, max_length=3000):
     return [
         dom_content[i: i + max_length] for i in range(0, len(dom_content), max_length)
     ]
